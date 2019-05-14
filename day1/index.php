132485cf8639c448d01955bcdbabc31c002d873f
Chen1fan <1812215600@qq.com>
2019-05-14 09:04:59 +0800

    第一天

---
+++ day1/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shinelon
+ * Date: 2019/5/10
+ * Time: 8:42
+ */
+$str = '123ABCabc*?-+';
+echo $str."<br>";
+$str = 'Have you ever gone shopping and';
+$arr = explode('',$str);
+print_r($arr);
